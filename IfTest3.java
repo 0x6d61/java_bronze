a5917bc2dfd450db8280473136168583d8f159ce
0x6d61 <0x6d61@gmail.com>
2021-05-20 18:09:46 +0900

    switchTest 追加

---
+++ IfTest3.java
@@ -0,0 +1,15 @@
+import java.io.Console;
+
+public class IfTest3 { 
+    public static void main(String[] args) throws Exception {
+        Console console = System.console();
+
+        int score = Integer.parseInt(console.readLine());
+
+        if (0 <= score && score <= 100) {
+            System.out.println("範囲内です");
+        }else{
+            System.out.println("範囲外です");
+        }
+    }
+}
