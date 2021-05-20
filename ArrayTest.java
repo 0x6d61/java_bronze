503011d85d3eefc0b3c97d7939e846e2611a4d99
0x6d61 <0x6d61@gmail.com>
2021-05-20 23:41:06 +0900

    foreach追加

---
+++ ArrayTest.java
@@ -0,0 +1,8 @@
+public class ArrayTest {
+    public static void main(String[] args) {
+        int[] array = {1,2,3};
+        int length = array.length;
+        System.out.println(length);
+        int[] a = new int[3];
+    }
+}
