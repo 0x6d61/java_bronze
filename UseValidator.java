6617d73b57a066d0569f8b49d85b30bae4fcca94
0x6d61 <0x6d61@gmail.com>
2021-05-21 11:11:48 +0900

    add UseValidator

---
+++ UseValidator.java
@@ -0,0 +1,9 @@
+public class UseValidator {
+
+  public static void main(String[] args) {
+    int score = 100;
+    if (Validator.isValidScore(score)) {
+      System.out.println("valid score");
+    }
+  }
+}
---
+++ Validator.java
@@ -0,0 +1,10 @@
+public class Validator {
+
+  public static boolean isValidScore(int score) {
+    if (score < 0 || 100 < score) {
+      return false;
+    }
+
+    return true;
+  }
+}
