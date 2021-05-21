4dde1c2591dbb277eac38311f525a4d918db0ce7
0x6d61 <0x6d61@gmail.com>
2021-05-21 22:57:58 +0900

    add shoppingsample

---
+++ ObjectArraySample.java
@@ -0,0 +1,13 @@
+public class ObjectArraySample {
+
+  public static void main(String[] args) {
+    Sample[] samples = new Sample[3];
+    samples[0] = new Sample();
+    samples[1] = new Sample();
+    samples[2] = new Sample();
+
+    for (Sample sample : samples) {
+      sample.hello();
+    }
+  }
+}
