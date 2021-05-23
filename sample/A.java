525361253816491d8769e31ccb01d2551f034af4
0x6d61 <0x6d61@gmail.com>
2021-05-23 18:56:18 +0900

    うぇい

---
+++ sample/A.java
@@ -0,0 +1,11 @@
+package sample;
+
+public class A {
+    public A() {
+        System.out.println("constructor");
+    }
+
+    protected void hello() {
+        System.out.println("hello");
+    }
+}
