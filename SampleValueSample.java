6e5228495bf14eea5144ecdbe0b055c8b43e1c0e
0x6d61 <0x6d61@gmail.com>
2021-05-25 06:10:45 +0900

    wei

---
+++ SampleValueSample.java
@@ -0,0 +1,7 @@
+public class SampleValueSample {
+    public static void main(String[] args) {
+        Client a = new Client(100,"xxx");
+        Client b = new Client(100,"xxx");
+        System.out.println(a.equals(b));
+    }
+}
